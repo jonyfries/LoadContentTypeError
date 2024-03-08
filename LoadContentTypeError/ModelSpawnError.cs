dd72f58a407aafafba189bd6e635a01c2ba58692
jonyfries <jonyfries@hotmail.com>
2024-03-08 11:31:46 -0500

    Load Content Type Error setup

---
+++ LoadContentTypeError/ModelSpawnError.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+using Stride.Rendering;
+
+namespace LoadContentTypeError
+{
+    public class ModelSpawnError : StartupScript
+    {
+        // Declared public member fields and properties will show in the game studio
+
+        public override void Start()
+        {
+            var model = Content.Load<Model>("Boat");
+            var modelComponent = new ModelComponent(model);
+            Entity.Add(modelComponent);
+        }
+    }
+}
---
+++ LoadContentTypeError/PrefabSpawnError.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+
+namespace LoadContentTypeError
+{
+    public class PrefabSpawnError : StartupScript
+    {
+        // Declared public member fields and properties will show in the game studio
+
+        public override void Start()
+        {
+            var boatPrefab = Content.Load<Prefab>("BoatCog");
+            Entity.AddChild(boatPrefab.Entities[0]);
+        }
+    }
+}
